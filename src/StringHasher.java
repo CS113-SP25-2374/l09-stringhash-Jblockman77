77ccd01b997d101631fbbfc7ee7a1b8c0d3aa1e9
Rafail Lishman <68488675+Jblockman77@users.noreply.github.com>
2025-04-15 06:43:42 +0000

    Will fix the Decode hashCode, when we get into the actual class.

--- src/StringHasher.java
+++ src/StringHasher.java
@@ -12,18 +12,35 @@ public class StringHasher {
         return hash;
     }
 
+    public static int cheapAndEasy(String s){
+        return s.hashCode();
+    }
+
     public static int javaHashCode(String s) {
         // implement the official hashCode for Strings!
         // s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
-        return 0;
+        int hash = 0;
+        int n = s.length()-1;
+        for(int i = 0; i< s.length(); i++){
+            if(hash != 2147483647){
+                hash += Math.pow(31, (n-i)) * s.charAt(i);
+            } else{
+                hash = -2147483647;
+                hash += Math.pow(31, (n-(i))) * s.charAt(i);
+            }
+        }
+        return hash;
     }
 
     public static void main(String[] args) {
         String[] testStrings = {
-                "hello", "olleh", "world", "dlrow", "java", "avaj",
-                "hash", "shah", "code", "decode"
+                "Cat", "hello", "olleh", "world", "dlrow", "java", "avaj",
+                "hash", "shah", "code", "decode",
         };
 
+
+        System.out.println("Cat".hashCode());
+        System.out.println("decode".hashCode());
         System.out.println("String\t\tbadHash\t\tbetterHash\t\thashCode");
         for (String s : testStrings) {
             System.out.printf("%-10s\t%-10d\t%-12d\t%-10d\n",
